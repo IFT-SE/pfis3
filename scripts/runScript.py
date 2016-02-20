76bcad04d67bc021685b9c6ada02bb0b5dd2e761
David Piorkowski <piorkoda@onid.orst.edu>
2016-02-20 00:15:55 -0800

    Updated runScript for batch runs to include xml param.

--- scripts/runScript.py
+++ scripts/runScript.py
@@ -9,13 +9,14 @@ def main():
 #     -o "/Users/Dave/Desktop/output/icsme15/p5"
 
     d = '/Users/Dave/Desktop/code'
-    p = '/Users/Dave/Documents/workspace/jEdit-2548764/src'
-#     p = '/Users/Dave/Desktop/code/p8l-vanillaMusic/src'
+#     p = '/Users/Dave/Documents/workspace/jEdit-2548764/src'
+    p = '/Users/Dave/Desktop/code/p8l-vanillaMusic/src'
     l = 'JAVA'
     s = '/Users/Dave/Desktop/code/pfis3/data/je.txt'
     o = '/Users/Dave/Desktop/output'
+    x = '/Users/Dave/Desktop/code/pfis3/data/javaAlgorithmOptions.xml'
     
-    db_dirName = 'icsme15'
+    db_dirName = 'fse16-mobile'
     
     d = os.path.join(d, db_dirName)
     db_fileNames = [f for f in os.listdir(d) if os.path.isfile(os.path.join(d, f)) and f.endswith('.db')]
@@ -31,7 +32,8 @@ def main():
                         '-p', p, \
                         '-l', l, \
                         '-s', s, \
-                        '-o', o_participant])
+                        '-o', o_participant, \
+                        '-x', x])
 
 
 if __name__ == '__main__':
