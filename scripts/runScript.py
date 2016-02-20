aa3c1421a9eed03d63d30ec88aab22ab3494066c
David Piorkowski <piorkoda@onid.orst.edu>
2016-02-20 00:04:44 -0800

    Added some scripts for running pfis3 over several databases and for combining the outputs of those runs into a single file with hit rates.

---
+++ scripts/runScript.py
@@ -0,0 +1,38 @@
+import os
+import subprocess
+
+def main():
+#     -d "/Users/Dave/Desktop/code/icsme15/p5.db" 
+#     -p "/Users/Dave/Documents/workspace/jEdit-2548764/src" 
+#     -l JAVA 
+#     -s "/Users/Dave/Desktop/code/pfis3/data/je.txt" 
+#     -o "/Users/Dave/Desktop/output/icsme15/p5"
+
+    d = '/Users/Dave/Desktop/code'
+    p = '/Users/Dave/Documents/workspace/jEdit-2548764/src'
+#     p = '/Users/Dave/Desktop/code/p8l-vanillaMusic/src'
+    l = 'JAVA'
+    s = '/Users/Dave/Desktop/code/pfis3/data/je.txt'
+    o = '/Users/Dave/Desktop/output'
+    
+    db_dirName = 'icsme15'
+    
+    d = os.path.join(d, db_dirName)
+    db_fileNames = [f for f in os.listdir(d) if os.path.isfile(os.path.join(d, f)) and f.endswith('.db')]
+    
+    for db in db_fileNames:
+        name = db[0:db.index('.')]
+        d_participant = os.path.join(d, db)
+        o_participant = os.path.join(o, db_dirName, name)
+        
+        subprocess.call(['python',
+                        '/Users/Dave/Desktop/code/pfis3/src/python/pfis3.py', \
+                        '-d', d_participant, \
+                        '-p', p, \
+                        '-l', l, \
+                        '-s', s, \
+                        '-o', o_participant])
+
+
+if __name__ == '__main__':
+    main()
