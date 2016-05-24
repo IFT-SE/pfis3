051f7837b00deda9d2783ab9b4d6db06e952b6e2
srutis90 <srutis90@gmail.com>
2016-05-23 17:35:15 -0700

    WIP | add variant info to MethodPatch

--- src/python/patches.py
+++ src/python/patches.py
@@ -1,3 +1,5 @@
+import uuid
+
 # TODO: Can this class and the HeaderData class be replaced/merged with the
 # FileNavigation class? They all seem to hold the same data...
 
@@ -7,7 +9,8 @@ class MethodPatch(object):
         self.fqn = fqn
         self.startOffset = -1
         self.length = -1
-        self.uuid = None
+        self.uuid = uuid.uuid1()
+        self.variantInfo = None
 
     def isOffsetInMethod(self, offset):
         endOffset = self.startOffset + self.length
@@ -20,3 +23,10 @@ class MethodPatch(object):
 
     def __str__(self):
         return str(self.startOffset) + ' to ' + str(self.startOffset + self.length - 1) + ': ' + self.fqn
+
+
+class VariantInfo(object):
+    def __init__(self, methodPath, startVariant, endVariant):
+        self.methodPath = methodPath
+        self.startVariant = startVariant
+        self.endVariant = endVariant
