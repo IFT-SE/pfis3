95797225b018f2e363a0cce3f8de1264a12cbe16
srutis90 <srutis90@gmail.com>
2016-06-28 16:32:22 -0700

    clean up code, use equivalent patches for predictions

--- src/python/algorithmFrequency.py
+++ src/python/algorithmFrequency.py
@@ -11,33 +11,33 @@ class Frequency(PredictiveAlgorithm):
         if navNumber < 1 or navNumber >= navPath.getLength():
             raise RuntimeError('makePrediction: navNumber must be > 0 and less than the length of navPath') 
         
-        self.__getOrderedFrequentMethods(navPath, navNumber)
+        self.__getOrderedFrequentMethods(pfisGraph, navPath, navNumber)
         navToPredict = navPath.navigations[navNumber]
         
         if not navToPredict.isToUnknown():
             # methodToPredict is the method we want to predict
             methodToPredict = navToPredict.toFileNav.methodFqn
             fromMethodFqn = navToPredict.fromFileNav.methodFqn
-            
-            for methodFqn in self.__methodFrequencies:
-                if methodFqn == methodToPredict:
-                    result = self.__methodFrequencies[methodFqn]
-                    
-                    sortedRanks = sorted(self.__methodFrequencies, key = lambda freq: self.__methodFrequencies[freq])
-                    firstIndex = self.getFirstIndex(sortedRanks, self.__methodFrequencies, result)
-                    lastIndex = self.getLastIndex(sortedRanks, self.__methodFrequencies, result)
-                    numTies = lastIndex - firstIndex + 1
-                    rankWithTies = self.getRankConsideringTies(firstIndex + 1, numTies)
-                    topPredictions = []
-                    
-                    if self.includeTop:
-                        topPredictions = self.getTopPredictions(sortedRanks, self.__methodFrequencies)
-                
-                    return Prediction(navNumber, rankWithTies, len(self.__methodFrequencies), 0,
-                                           fromMethodFqn,
-                                           methodToPredict,
-                                           navToPredict.toFileNav.timestamp,
-                                           topPredictions)
+            methodToPredictEquivalentNode = pfisGraph.getFqnOfEquivalentNode(methodToPredict)
+
+            if methodToPredictEquivalentNode in self.__methodFrequencies.keys():
+                result = self.__methodFrequencies[methodToPredictEquivalentNode]
+                sortedRanks = sorted(self.__methodFrequencies, key = lambda freq: self.__methodFrequencies[freq])
+                firstIndex = self.getFirstIndex(sortedRanks, self.__methodFrequencies, result)
+                lastIndex = self.getLastIndex(sortedRanks, self.__methodFrequencies, result)
+                numTies = lastIndex - firstIndex + 1
+                rankWithTies = self.getRankConsideringTies(firstIndex + 1, numTies)
+                topPredictions = []
+
+                if self.includeTop:
+                    topPredictions = self.getTopPredictions(sortedRanks, self.__methodFrequencies)
+
+                return Prediction(navNumber, rankWithTies, len(self.__methodFrequencies), 0,
+                                  fromMethodFqn,
+                                  methodToPredict,
+                                  navToPredict.toFileNav.timestamp,
+                                  topPredictions)
+
         
         return Prediction(navNumber, 999999, len(self.__methodFrequencies), 0,
                           str(navToPredict.fromFileNav), 
@@ -45,13 +45,13 @@ class Frequency(PredictiveAlgorithm):
                           navToPredict.toFileNav.timestamp)
         
     
-    def __getOrderedFrequentMethods(self, navPath, navNum):
-        
+    def __getOrderedFrequentMethods(self, pfisGraph, navPath, navNum):
         for i in range(navNum + 1):
             nav = navPath.navigations[i]
             if nav.fromFileNav is not None:
                 visitedMethod = nav.fromFileNav.methodFqn
-                if visitedMethod in self.__methodFrequencies:
-                    self.__methodFrequencies[visitedMethod] += 1
+                visitedMethodEquivalent = pfisGraph.getFqnOfEquivalentNode(visitedMethod)
+                if visitedMethodEquivalent in self.__methodFrequencies:
+                    self.__methodFrequencies[visitedMethodEquivalent] += 1
                 else:
-                    self.__methodFrequencies[visitedMethod] = 1
+                    self.__methodFrequencies[visitedMethodEquivalent] = 1
